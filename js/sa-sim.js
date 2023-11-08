5d48080bce638665bbccd2deb79fa29750e2bb70
Александр Озеров <cdg.axel@gmail.com>
2023-11-08 03:45:30 +0300

    Pycharm

--- js/sa-sim.js
+++ js/sa-sim.js
@@ -3,7 +3,7 @@ let simRunning = false;
 let targetPoints;
 let firstTier;
 let simulationCount;
-let tiers = ["E-", "E", "E+", "D-", "D", "D+", "C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+", "S"]
+let tiers = ["E-", "E", "E+", "D-", "D", "D+", "C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+", "<script>"]
 let chances = [4.3, 19.8, 28.8, 20, 9.2, 4.8, 4.4, 4.3, 2.13, 1.62, .55, .0745, .015, .0065, .0025, .0015];
 let points = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 let gems = [10, 15, 20, 30, 50, 70, 100, 150, 200, 300, 600, 1800, 8000, 15000, 25000, 50000];
@@ -11,26 +11,21 @@ let totalPoints;
 let minPoints;
 let maxPoints;
 const elCount = chances.length;
+const maxProb = 1000000;
 let intervals = new Array(elCount);
 let tierDrop = new Array(elCount);
+let fullProb = new Array(maxProb)
 let isRussian = false;
-
 
 const lsPrefix = 'sa_sim_';
 
-function esc(unsafe) {
-    return String(unsafe)
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
- }
+function esc(parts, ...params){
+    return parts[0] + params.map((p,i) => String(p).replace(/(&|<|>|"|')/g, s=>({"<": "&lt;", ">": "&gt", '"':"&quot;","'":"&#039;"})[s]) + parts[i+1]).join('');
+}
 
 function generateTable() {
-    document.getElementById('resTableBody').innerHTML = tiers.map(
-        (tier, i) =>
-        `<tr><th scope="row">${esc(tier)}</th><td>${chances[i]}</td><td>${points[i]}</td><td id="avRes${i}">-</td></tr>`
+    document.getElementById('resTableBody').innerHTML = tiers.map((tier, i) =>
+        esc`<tr><th scope="row">${tier}</th><td>${chances[i]}</td><td>${points[i]}</td><td id="avRes${i}">-</td></tr>`
     ).join('')
 }
 
@@ -77,7 +72,8 @@ function simulationStep() {
     let dices = 0;
     let bonus = 0;
     while (collected < targetPoints) {
-        let idx = binSearch(Math.random());
+        // let idx = binSearch(Math.random());
+        let idx = fullProb[Math.floor(maxProb * Math.random())];
         collected += points[idx];
         dices += 1;
         tierDrop[idx] += 1;
@@ -94,8 +90,8 @@ function simulationStep() {
 
 function simulationBlock() {
 	if (simRunning && simNum < simulationCount) {
-	    // one block is 100 simulation steps
-		for (let i = 0; i < 100; i++) {
+	    // one block is 1000 simulation steps
+		for (let i = 0; i < 1000; i++) {
 			simNum++;
 			simulationStep();
 		}
@@ -115,17 +111,19 @@ function runSimulation(isRus = false) {
         firstTier = parseInt(document.getElementById('edFirstTier').value);
         simulationCount = parseInt(document.getElementById('edSimulationCount').value);
 
-
         simNum = 0;
         totalPoints = 0;
         minPoints = 100000;
         maxPoints = 0;
 
         let elSum = 0;
+        let lastEl = 0;
         for (let i = 0; i < elCount; i++) {
             elSum += chances[i];
             intervals[i] = elSum / 100;
             tierDrop[i] = 0;
+            let fillCnt = Math.floor(elSum * maxProb / 100);
+            for (; lastEl < fillCnt; lastEl++) fullProb[lastEl] = i;
         }
 
 		setTimeout(simulationBlock, 1);
